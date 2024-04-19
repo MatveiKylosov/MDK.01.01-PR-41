f60acbf093a5b45b2f8132caf13e7799df762010
MatveiKylosov <matveikyl@mail.ru>
2024-04-20 00:25:55 +0500

    Реализованное представление модели таймера. Kylosov

---
+++ ViewModell/VMStopwatch.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Threading;
+using Stopwatch = TimeLord_MVVM_Kylosov.Modell.Stopwatch;
+
+namespace TimeLord_MVVM_Kylosov.ViewModell
+{
+    public class VMStopwatch : INotifyPropertyChanged
+    {
+        public Stopwatch StopWatch { get; set; }
+        private DispatcherTimer Timer = new DispatcherTimer()
+        {
+            Interval = new System.TimeSpan(0, 0, 1)
+        };
+        public VMStopwatch()
+        {
+            StopWatch = new Stopwatch() { Work = false, Time = 0 };
+            Timer.Tick += Timer_Tick;
+            Timer.Start();
+        }
+        private void Timer_Tick(object sender, System.EventArgs e)
+        {
+            if (StopWatch.Work) 
+                StopWatch.Time++;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            
+        }
+    }
+}
